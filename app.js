233691bd72ec95ae871bbabbb47083995f51ab27
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-01-29 13:54:02 -0500

    check

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('services.json')
 const middlewares = jsonServer.defaults()
+
+require('dotenv').config()
  
+let port  =  process.env.PORT || 3001
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log(`Mock Services runnig  port ${port}`)
 })
